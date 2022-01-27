e15c10d8a2bbd82360d44e8f4338ba9319ba3d5d
RiiFU <di.22rus@yandex.ru>
2022-01-27 21:57:35 +0700

    Добавьте файлы проекта.

---
+++ Lesson2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double sred;
+            Console.WriteLine("Введите минимальную температуру за сутки");
+            var min = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите максимальную температуру за сутки");
+            var max = Convert.ToDouble(Console.ReadLine());
+            sred = (min + max) / 2;
+            Console.WriteLine($"Среднесуточная температура = {sred}");
+            Console.ReadKey();
+        }
+    }
+}
