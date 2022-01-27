101af5ad2c2e6bf279154eda4b26a479bec197a2
RiiFU <di.22rus@yandex.ru>
2022-01-27 23:54:09 +0700

    Задание4

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -10,13 +10,34 @@ namespace Lesson2
     {
         static void Main(string[] args)
         {
-           double sred;
-            Console.WriteLine("Введите минимальную температуру за сутки");
-            var min = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите максимальную температуру за сутки");
-            var max = Convert.ToDouble(Console.ReadLine());
-            sred = (min + max) / 2;
-            Console.WriteLine($"Среднесуточная температура = {sred}");
+
+            string name = "        ООО \"Образец\"          ";
+            string KKM = "000012345";
+            DateTime dateAndTime = new DateTime(2011, 10, 11, 11, 18, 22);
+            string INN = "001234567890";
+            string numCheque = "#2222";
+            string kefir = "=25.00";
+            string smetana = "=55.00";
+            string sum = "=80.00";
+            string nal = "100";
+            string surrender = "20";
+            string eclz = "1234567890";
+
+            Console.WriteLine($"+---------------------------------+");
+            Console.WriteLine($"|                                 |");
+            Console.WriteLine($"|{name}  |");
+            Console.WriteLine($"|ККМ {KKM}   ИНН {INN} |");
+            Console.WriteLine($"|{dateAndTime}      Кассир 1|");
+            Console.WriteLine($"|ПРОДАЖА                     {numCheque}|");
+            Console.WriteLine($"|Кефир                      {kefir}|");
+            Console.WriteLine($"|Сметана                    {smetana}|");
+            Console.WriteLine($"|Всего                      {sum}|");
+            Console.WriteLine($"|Наличными                     {nal}|");
+            Console.WriteLine($"|Сдача                          {surrender}|");
+            Console.WriteLine($"+---------------------------------+");
+            Console.WriteLine($"|ЭКЛЗ {eclz}                  |");
+            Console.WriteLine($"+---------------------------------+");
+
             Console.ReadKey();
         }
     }
